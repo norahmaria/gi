69048e5a8f502fb28b871404f724268a9def29d5
Norah Maria Hareide Nilsen <norahmaria@Norahs-MBP.domain_not_set.invalid>
2021-08-29 09:43:15 +0200

    React emoji

--- src/embeds/languageRoles.ts
+++ src/embeds/languageRoles.ts
@@ -3,14 +3,14 @@ import Discord from 'discord.js'
 import { yellow } from '../constants/colors'
 import languages from '../constants/languages/languages'
 
-export const languageRolesEmbedId = '881399146597187605'
+export const languageRolesEmbedId = '881443729951563806'
 const langDescriptions = languages.map(lang => `${lang.emoji} for **${lang.desc}**`).join('\n\n')
 
 const languageRolesEmbed = () => {
   return new Discord.MessageEmbed()
     .setColor(yellow)
     .setTitle('Pick your technologies')
-    .setDescription(`Please **react :react:** with the languages you've learnt or are currently trying to learn, this will give you access to the different help channels. \n\n ${langDescriptions}`)
+    .setDescription(`Please **react <:react:877518299573911572>** with the languages you've learnt or are currently trying to learn, this will give you access to the different help channels. \n\n ${langDescriptions}`)
 }
 
 export default languageRolesEmbed
